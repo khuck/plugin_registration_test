229bedd6950c1e3aaee00d7ec1edc5a2db7ab686
Kevin Huck <khuck@cs.uoregon.edu>
2020-01-07 16:00:09 -0800

    Making sure that both tools check for the existence of the registration function

--- tool1.c
+++ tool1.c
@@ -18,17 +18,23 @@ void tool_finalize(void) {
     printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
 }
 
+/* What's happening here?  Well, we check to see if
+   the registration function is our own weak (and undefined)
+   version of the function, or if the plugin library exists
+   in the application.  this checks the case when this tool
+   is linked into an appliation without the plugin support. */
+
 static void initme(void) {
     plugin_pointers_t pointers;
     plugin_register_t reg_function;
-    printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
-    memset(&pointers, 0, sizeof(plugin_pointers_t));
-    pointers.tool_name = "one";
-    pointers.init = &tool_init;
-    pointers.function = &tool_function;
-    pointers.finalize = &tool_finalize;
     reg_function = &register_tool;
     if (reg_function != NULL) {
+        printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
+        memset(&pointers, 0, sizeof(plugin_pointers_t));
+        pointers.tool_name = "one";
+        pointers.init = &tool_init;
+        pointers.function = &tool_function;
+        pointers.finalize = &tool_finalize;
         reg_function(&pointers);
     }
 }
--- tool2.c
+++ tool2.c
@@ -18,15 +18,25 @@ void tool2_finalize(void) {
     printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
 }
 
+/* What's happening here?  Well, we check to see if
+   the registration function is our own weak (and undefined)
+   version of the function, or if the plugin library exists
+   in the application.  this checks the case when this tool
+   is linked into an appliation without the plugin support. */
+
 static void initme(void) {
     plugin_pointers_t pointers;
-    printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
-    memset(&pointers, 0, sizeof(plugin_pointers_t));
-    pointers.tool_name = "two";
-    pointers.init = &tool2_init;
-    pointers.function = &tool2_function;
-    pointers.finalize = &tool2_finalize;
-    register_tool(&pointers);
+    plugin_register_t reg_function;
+    reg_function = &register_tool;
+    if (reg_function != NULL) {
+        printf("%s %s\n", __FILE__, __PLUGIN_FUNCTION__);
+        memset(&pointers, 0, sizeof(plugin_pointers_t));
+        pointers.tool_name = "two";
+        pointers.init = &tool2_init;
+        pointers.function = &tool2_function;
+        pointers.finalize = &tool2_finalize;
+        reg_function(&pointers);
+    }
 }
 
 __attribute__((visibility("default")))
